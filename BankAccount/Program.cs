7b5b9eb63e46c08c5aa731a20579293484d16971
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-11-16 18:21:30 +0100

    Prototype is finished Blik() to fix

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,1 +1,15 @@
 ﻿using BankAccount;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Threading;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BankManager bankManager = new BankManager();
+        bankManager.Run();
+
+    }
+}
