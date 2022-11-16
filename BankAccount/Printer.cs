8b031efc4723a0fe3421563126eed469567aee35
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-11-16 15:20:48 +0100

    Dodaj pliki projektów.

---
+++ BankAccount/Printer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class Printer : IPrinter
+    {
+        public void Print(Account account)
+        {
+            Console.WriteLine("Dane konta: {0}", account.AccountNumber);
+            Console.WriteLine("Typ: {0}", account.GetType());
+            Console.WriteLine("Saldo: {0}zł", account.Balance);
+            Console.WriteLine("Imię właściciela: {0}", account.FirstName);
+            Console.WriteLine("Nazwisko właściciela: {0}", account.LastName);
+            Console.WriteLine("PESEL właściciela: {0}", account.Pesel);
+            Console.WriteLine();
+        }
+
+    }
+}
